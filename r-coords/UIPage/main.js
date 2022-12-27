78d70b95894416804731581089fbdcf89efd7916
Vumono <87247208+Vumono@users.noreply.github.com>
2022-12-27 19:11:06 +0100

    Update main.js

--- r-coords/UIPage/main.js
+++ r-coords/UIPage/main.js
@@ -6,12 +6,12 @@ $(() => {
             $('body').css('display', 'block')
 
             $('.all').append(` 
-                <div class="coords"> <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vector3(${v._charPos.x}, ${v._charPos.y}, ${v._charPos.z}) </span> </div>
-                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vector4(${v._charPos.x}, ${v._charPos.y}, ${v._charPos.z}, ${v._charHeading}) </span>  </div>
-                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > x = ${v._charPos.x}, y = ${v._charPos.y}, z = ${v._charPos.z} </span>  </div>
-                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > x = ${v._charPos.x}, y = ${v._charPos.y}, z = ${v._charPos.z}, h = ${v._charHeading} </span>  </div>
-                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vec3(${v._charPos.x}, ${v._charPos.y}, ${v._charPos.z}) </span>  </div>
-                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vec4(${v._charPos.x}, ${v._charPos.y}, ${v._charPos.z}, ${v._charHeading}) </span>  </div>
+                <div class="coords"> <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vector3(${v._charPosx}, ${v._charPosy}, ${v._charPosz}) </span> </div>
+                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vector4(${v._charPosx}, ${v._charPosy}, ${v._charPosz}, ${v._charHeading}) </span>  </div>
+                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > x = ${v._charPosx}, y = ${v._charPosy}, z = ${v._charPosz} </span>  </div>
+                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > x = ${v._charPosx}, y = ${v._charPosy}, z = ${v._charPosz}, h = ${v._charHeading} </span>  </div>
+                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vec3(${v._charPosx}, ${v._charPosy}, ${v._charPosz}) </span>  </div>
+                <div class="coords">  <span id="copy"><i class="fa-solid fa-copy"></i></span> <span id="text" > vec4(${v._charPosx}, ${v._charPosy}, ${v._charPosz}, ${v._charHeading}) </span>  </div>
                 
 
                 `) 
